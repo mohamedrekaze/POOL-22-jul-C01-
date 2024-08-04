8429a77a0888bf9a911e6cdc8dffabe7b145ad58
Abdellah Bagueri <73394292+abdollah34@users.noreply.github.com>
2024-08-04 10:24:09 +0100

    all exams

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abagueri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 12:41:05 by abagueri          #+#    #+#             */
+/*   Updated: 2024/07/24 16:37:11 by abagueri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	reverse;
+
+	reverse = 'z';
+	while (reverse >= 'a')
+	{
+		write(1, &reverse, 1);
+		reverse--;
+	}
+}
