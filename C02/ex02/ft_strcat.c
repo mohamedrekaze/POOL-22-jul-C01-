e327cf3bd1395062f88d7a5a105a20190d812962
Abdellah Bagueri <73394292+abdollah34@users.noreply.github.com>
2024-08-07 16:37:54 -0400

    new edite

---
+++ C02/ex02/ft_strcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abagueri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 12:13:16 by abagueri          #+#    #+#             */
+/*   Updated: 2024/08/07 15:59:16 by abagueri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[j] == "\0";
+	return (dest);
+}
+//int main(void)
+//{
+// char	dest[50] = "hala";
+// char	src[] = "barca";
+//	printf("%s ",ft_strcat(dest, src));
+//}
