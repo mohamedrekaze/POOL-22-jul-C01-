7599b0cf244de50f8daa75026c725d3ab5854456
mohamedrekaze <74571217+mohamedrekaze@users.noreply.github.com>
2024-08-15 16:02:10 +0100

    Update ft_strstr.c

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -16,19 +16,21 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
-	j = 0;
-	while (str[i] != '\0')
+	if (to_find[i] == '\0')
+		return (str);
+	while (str[i])
 	{
-		while (to_find[j] == str[i + j] && str[i + j] != '\0')
+		j = 0;
+		if (str[i] == to_find[j])
 		{
-			j++;
-		}
-		if (to_find[j] == '\0')
-		{
-			return (str + i);
+			while (str[i + j] == to_find[j])
+			{
+				if (to_find[j + 1] == '\0')
+					return (&str[i]);
+				j++;
+			}
 		}
 		i++;
-		j = 0;
 	}
 	return (0);
 }
